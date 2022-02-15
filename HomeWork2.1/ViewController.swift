4f324f752b52dc22f1e10c2a94862e342f53dfaa
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-02-15 21:30:39 +0300

    type(rgbApplication): init

--- HomeWork2.1/ViewController.swift
+++ HomeWork2.1/ViewController.swift
@@ -1,19 +1,86 @@
-//
-//  ViewController.swift
-//  HomeWork2.1
-//
-//  Created by Paul on 15.02.22.
-//
+// Я захардкодил ширину лейбла, отображающего значение слайдера :(
 
 import UIKit
 
+// Plan:
+// 1. IBOutlets
+// 2. Properties
+// 3. Life Cycle
+// 4. Methods
+// 5. Action
+
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redSliderName: UILabel!
+    @IBOutlet var greenSliderName: UILabel!
+    @IBOutlet var blueSliderName: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redSliderCounter: UILabel!
+    @IBOutlet var greenSliderCounter: UILabel!
+    @IBOutlet var blueSliderCounter: UILabel!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupUI()
+    }
+    
+    
+    // MARK: - Methods
+    func setupUI() {
+        rgbView.layer.cornerRadius = 15
+
+        sliderNameDesign()
+        sliderCounterDesign()
+        sliderDesign()
+    }
+    
+    func sliderNameDesign() {
+        redSliderName.layer.masksToBounds = true
+        greenSliderName.layer.masksToBounds = true
+        blueSliderName.layer.masksToBounds = true
+        
+        redSliderName.layer.cornerRadius = 5
+        greenSliderName.layer.cornerRadius = 5
+        blueSliderName.layer.cornerRadius = 5
     }
 
+    func sliderCounterDesign() {
+        redSliderCounter.layer.masksToBounds = true
+        greenSliderCounter.layer.masksToBounds = true
+        blueSliderCounter.layer.masksToBounds = true
+        
+        redSliderCounter.layer.cornerRadius = 5
+        greenSliderCounter.layer.cornerRadius = 5
+        blueSliderCounter.layer.cornerRadius = 5
+    }
+    
+    func sliderDesign() {
+        redSlider.layer.cornerRadius = 15
+        greenSlider.layer.cornerRadius = 15
+        blueSlider.layer.cornerRadius = 15
+    }
+    
+    // MARK: - Action
+    @IBAction func redSliderAction() {
+        redSliderCounter.text = String(redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenSliderCounter.text = String(greenSlider.value)
 
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueSliderCounter.text = String(blueSlider.value)
+    }
 }
 
